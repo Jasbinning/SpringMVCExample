d5f4a793ea7b6ecd444b74d3672a853dce137ea9
Jasbinning <jassbinning4@gmail.com>
2022-05-03 10:33:08 -0400

    first project upload

---
+++ src/main/java/com/blit/SpringMVCExample/Repository/InvoiceRepository.java
@@ -0,0 +1,17 @@
+package com.blit.SpringMVCExample.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.blit.SpringMVCExample.entity.Invoice;
+
+
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+
+
+
+
+
+}
---
+++ src/test/java/com/blit/SpringBootMVCExample/SpringMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blit.SpringBootMVCExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
